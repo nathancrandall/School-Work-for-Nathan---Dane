afd483b997b683106fca973a9b9183a598687265
Nathan Crandall <cbcramps@gmail.com>
2012-01-24 16:51:34 -0800

    updated

--- hw2 cs140.cpp
+++ hw2 cs140.cpp
@@ -1,21 +1,37 @@
-#include <iostream>
-#include <cmath>
+/* CS 140
+ * Assignment 1 : Matrix Vector Multiplication and the Power Method
+ * Group members : Nathan Crandall , Dane Pitkin
+ * */
 
-using namespace std;
+/* This file provides the placeholder function definitions, where you will be
+ * implementing the assignment algorithms. You will be required to turn in
+ * only this file during the submission, where it will be compiled together
+ * with our main function and tested. It is therefore required that you keep the
+ * function declaration formats unchanged.
+ */
+
+#include "powermethod.h"
 
 void generatematrix(double * mat, int size)
-{  
+{
+  int nprocs, rank;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  int tempMatrix[size*size];
+
   for(int i = 0; i < size; i++)
   {
-          for(int j = 0; j < size; j++)
-          {
-                  mat[(i*size)+j] = 0;
-          }
-          for(int k = 0; k <= i; k++)
-          {
-                  mat[(i*size)+k] = i+1;  
-          }
+    for(int j = 0; j < size; j++)
+      tempMatrix[(i*size)+j] = 0;
+    for(int k = 0; k <= i; k++)
+      tempMatrix[(i*size)+k] = i+1;
   }
+
+  int offset = ((size*size)/nprocs)*rank;
+  for(int i = 0; i < (size*size)/nprocs; i++)
+    mat[i] = tempMatrix[i+offset];
 }
 
 // Subroutine to generate a random vector
@@ -29,70 +45,58 @@ double norm2(double *vector, int size)
 {
   double total = 0;
   for(int i = 0; i < size; i++)
-  {
-    total += (vector[i] * vector[i]);
-  }
+    {
+      total += (vector[i] * vector[i]);
+    }
   return sqrt(total);
 }
-
 void matVec(double *matrix, double *vector, int mSize, int vSize, double *result)
 {
+  int nprocs, rank;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+
   if(mSize != vSize)
   {
     printf("cannot multiply together.\n");
     return;
   }
-  double *newVec = new double[mSize];
+
+
+
+  double temp[mSize*mSize];
+
+  for(int i = 0; i < nprocs;i++)
+    MPI_Gather(matrix, (mSize*mSize)/nprocs, MPI_DOUBLE, temp ,(mSize*mSize)/nprocs, MPI_DOUBLE,i, MPI_COMM_WORLD);
+
+
+ double *newVec = (double*)malloc(sizeof(double)*mSize);
   for(int i = 0; i < mSize; i++)
-          newVec[i] = 0;
+    newVec[i] = 0;
   for(int i = 0; i < mSize; i++)
-  {
-    for(int j = 0; j < vSize; j++)
     {
-      newVec[i] += matrix[(i*mSize)+j] * vector[j];
+      for(int j = 0; j < vSize; j++)
+        {
+          newVec[i] += temp[(i*mSize)+j] * vector[j];
+        }
     }
-  }
   for(int i = 0; i < mSize; i++)
-          result[i] = newVec[i];
-  delete [] newVec;
+    result[i] = newVec[i];
+  free(newVec);
 }
 
 
 double powerMethod(double * mat, double * x, int size, int iter)
-{  
+{
   double newX[size];
   for(int i = 0; i < iter; i++)
-  {
-          for(int j = 0; j < size; j++)
-          {
-                  x[j] = x[j] / norm2(x, size);
-                  
-          }
-          matVec(mat, x, size, size, x);
-  }
+    {
+      for(int j = 0; j < size; j++)
+        {
+          x[j] = x[j] / norm2(x, size);
 
-  return norm2(x, size);
-}
-
-
-int main()
-{
-    double matrix[36] = {1,0,0,0,0,0,2,2,0,0,0,0,3,3,3,0,0,0,4,4,4,4,0,0,5,5,5,5,5,0,6,6,6,6,6,6};
-    double vec[6];
-    double spectral_radius;
-    int size = 6;
-    int iter = 1000;
-    
-    //generatematrix(matrix, 4);
-    generatevec(vec, 6);
-    
-    
-    spectral_radius = powerMethod(matrix,vec,size,iter);
-    
-    cout << "spectral radius = " << spectral_radius << endl;
-    
-    
-    
-    system("PAUSE");
-    return 0;
-}
+        }
+      matVec(mat, x, size, size, x);
+    }
